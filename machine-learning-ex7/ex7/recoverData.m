52c5c75b96fca3cd297a22d105871f6150451545
akrentsel <alex.krentsel@gmail.com>
2016-12-29 09:39:15 +0700

    Finished ex7

--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -21,7 +21,9 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
+% Z = X * U;
 
+X_rec = Z * U(:,1:K)';
 
 % =============================================================
 
